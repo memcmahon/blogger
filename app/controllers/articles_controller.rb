28751be4e699d22a21b75da18f9da9817bd081d1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-03 14:14:05 -0700

    Adds delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,9 +19,9 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
-  def delete
-    @article = Article.find(params[:id])
-    @article.destroy
+  def destroy
+    Article.destroy(params[:id])
+    redirect_to articles_path
   end
 
   private
