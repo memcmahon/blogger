c316b79800f787faf4df515b152b5966cc208115
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-03 14:27:36 -0700

    Adds test for article edit

---
+++ spec/features/user_can_edit_existing_articles_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe "user can edit an article" do
+  describe "they visit article/id" do
+    it "has a link to edit the article" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+
+      visit article_path(article_1)
+      click_link "Edit"
+
+      expect(current_path).to eq edit_article_path
+    end
+  end
+
+  describe "they visit article/id/edit" do
+    it "edits the existing article" do
+      article_1 = Article.create!(title: "Title 1", body: "Body 1")
+
+      visit edit_article_path(article_1)
+
+      fill_in "article[title]", with: "Edited Title!"
+      fill_in "article[body]", with: "Edited Body!"
+      click_on "Save"
+
+      expect(current_path).to eq(show_article_path(article_1))
+      expect(page).to have_content("Edited Title!")
+      expect(page).to have_content("Edited Body!")
+    end
+  end
+end
