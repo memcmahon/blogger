59277192b1303151dd347a8cb7503635eab627b7
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-03 15:36:43 -0700

    Adds tests for comment view on articles

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,6 +4,8 @@ describe "user sees a sigle article" do
   describe "they visit article/id" do
     it "shows article information" do
       article = Article.create!(title: "Article One", body: "This is the first article!")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
       visit articles_path
       click_link article.title
@@ -11,6 +13,11 @@ describe "user sees a sigle article" do
       expect(current_path).to eq("/articles/#{article.id}")
       expect(page).to have_content("Article One")
       expect(page).to have_content("This is the first article!")
+      expect(page).to have_content("Me")
+      expect(page).to have_content("You")
+      expect(page).to have_content("Commenty comments")
+      expect(page).to have_content("So much to say")
+
     end
   end
 end
