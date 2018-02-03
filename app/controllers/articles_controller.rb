4e773b03eaf3e43f1c28e1d1562ad5dd6b8a1bc7
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-03 14:57:15 -0700

    Adds edit functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
 
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -19,14 +19,29 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+  end
+
+  def update
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
+
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy
+
     redirect_to articles_path
   end
 
   private
 
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
   end
+
 end
--- spec/features/user_can_edit_existing_articles_spec.rb
+++ spec/features/user_can_edit_existing_articles_spec.rb
@@ -8,7 +8,7 @@ describe "user can edit an article" do
       visit article_path(article_1)
       click_link "Edit"
 
-      expect(current_path).to eq edit_article_path
+      expect(current_path).to eq edit_article_path(article_1)
     end
   end
 
@@ -20,9 +20,9 @@ describe "user can edit an article" do
 
       fill_in "article[title]", with: "Edited Title!"
       fill_in "article[body]", with: "Edited Body!"
-      click_on "Save"
+      click_on "Update Article"
 
-      expect(current_path).to eq(show_article_path(article_1))
+      expect(current_path).to eq(article_path(article_1))
       expect(page).to have_content("Edited Title!")
       expect(page).to have_content("Edited Body!")
     end
